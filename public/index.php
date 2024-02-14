f499e55eeb62d38343f334a467bcb1f6cbac6293
charly0475 <charly.ongemba.ngongo@cf2000.onmicrosoft.com>
2024-02-15 00:05:49 +0100

    deux

--- public/index.php
+++ public/index.php
@@ -5,12 +5,17 @@
 // Connexion à la base de donnée
 
 // Si le formulaire a été envoyé
+require_once "../config.php";
 
-    // On insert dans la table `informations` si valide
+
+// On insert dans la table `informations` si valide
 
 // on récupère toutes les entrées de la table
 // `informations`
+require_once "../controller/routerController.php";
 
 // on charge le template qui affiche la vue
+include_once "../view/$route";
 
 // on ferme la connexion 
+
