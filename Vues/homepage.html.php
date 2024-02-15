ff668f32e80d875b161a423d6fc5fd9dff0e3380
charly0475 <charly.ongemba.ngongo@cf2000.onmicrosoft.com>
2024-02-15 09:55:12 +0100

    dex

--- Vues/homepage.html.php
+++ Vues/homepage.html.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
     <meta name="description" content="Group2ti2exercice | Accueil"/>
     <meta name="author" content="Pierre Sandron"/>
-    <title>Group2ti2exercice | Accueil</title>
+    <title>informations| vue</title>
     <!-- Favicon-->
     <link rel="icon" type="image/x-icon" href="img/favicon.ico"/>
     <!-- Core theme CSS (includes Bootstrap)-->
@@ -19,7 +19,7 @@ include "menu.html.php";
 <!-- Page content-->
 <div class="container">
     <div class="text-center mt-5">
-        <h1>Group2ti2exercice | Accueil</h1>
+        <h1>Informations | vue</h1>
         <p class="lead">A complete project boilerplate built with Bootstrap</p>
         <p>Bootstrap v5.2.3</p>
     </div>
